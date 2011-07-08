ae6da033435dbafd3426a9fb738e557f64092bd6
Mitesh Jain <mitijain123@gmail.com>
2011-07-08 14:55:19 +0530

    added the tests

--- test/unit/artist_test.rb
+++ test/unit/artist_test.rb
@@ -1,7 +1,10 @@
-require 'test_helper'
+ENV["RAILS_ENV"] = "test"
+require File.expand_path('../../../config/environment', __FILE__)
+require 'rails/test_help'
 
 class ArtistTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    Artist.create(:name => "Mrs")    
+    assert !Artist.find_by_name("Mrs").last.blank?
+  end
 end
